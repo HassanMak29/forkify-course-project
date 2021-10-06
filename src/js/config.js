976f5225559099398569102a1f0eb1a2fb0d93c1
hassanMak29 <hassan_makhloufi@yahoo.com>
2021-10-06 20:51:11 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 12;
+export const KEY = '20158955-7e6b-4c79-8f6a-168407c4d711';
+export const MODAL_CLOSE_SEC = 2.5;
